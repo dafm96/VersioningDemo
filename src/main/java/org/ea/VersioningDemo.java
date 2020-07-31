c19e32f3fe2de897135093f31e0880f56ead0049
David <david.moura@celfocus.com>
2020-07-31 20:07:51 +0100

    NEW SUPER CHANGES

--- src/main/java/org/ea/VersioningDemo.java
+++ src/main/java/org/ea/VersioningDemo.java
@@ -5,6 +5,7 @@ package org.ea;
  */
 public class VersioningDemo {
     public static void main(String args[]) {
+        //CHANGES FOR NEW SUPER FEATURE
         System.out.println("Hello explorers.");
         System.out.println();
         System.out.println("This program is running version "+
