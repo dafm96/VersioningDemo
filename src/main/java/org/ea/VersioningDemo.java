a57c6991ec99dd45d8a4b3b713be059262e030dd
David <david.moura@celfocus.com>
2020-08-03 10:31:21 +0100

    removed changes to test tag

--- src/main/java/org/ea/VersioningDemo.java
+++ src/main/java/org/ea/VersioningDemo.java
@@ -4,11 +4,11 @@ package org.ea;
  * Versioning demo created by Daniel Persson
  */
 public class VersioningDemo {
-    public static void main(String args[]) {
-        //CHANGES FOR NEW SUPER FEATURE
-        System.out.println("Hello explorers.");
-        System.out.println();
-        System.out.println("This program is running version "+
-                VersioningDemo.class.getPackage().getImplementationVersion());
-    }
+
+  public static void main(String args[]) {
+    System.out.println("Hello explorers.");
+    System.out.println();
+    System.out.println("This program is running version " +
+        VersioningDemo.class.getPackage().getImplementationVersion());
+  }
 }
