d393bba2c8ea32add41097fd245f80c2016a51ee
David <david.moura@celfocus.com>
2020-08-03 11:52:01 +0100

    new feature

--- src/main/java/org/ea/VersioningDemo.java
+++ src/main/java/org/ea/VersioningDemo.java
@@ -4,7 +4,7 @@ package org.ea;
  * Versioning demo created by Daniel Persson
  */
 public class VersioningDemo {
-
+  //test new feature
   public static void main(String args[]) {
     System.out.println("Hello explorers.");
     System.out.println();
